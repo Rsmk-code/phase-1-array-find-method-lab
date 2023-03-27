0b5dea8feea7683fa3889dbeb0a9549aceb636f7
Ahmad Ramin Soleyman kheyl <sramin963@gmail.com>
2023-03-27 19:05:19 +0300

    Assingment is done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+//code your solution here
+const record = [
+    {year: '2015', result: 'W'},
+    {year: '2014', result: 'N/A'},
+    {year: '2013', result: 'L'},
+];
+  
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win? win.year : undefined;
+  }
+  
+  const yearOfWin = superbowlWin(record);
+  if (yearOfWin) {
+    console.log(`The Denver Broncos won the Superbowl in ${yearOfWin}`);
+  } else {
+    console.log("The Denver Broncos have never won the Superbowl");
+  }
